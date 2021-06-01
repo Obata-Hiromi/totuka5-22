111c87b7047f434ad66e8e33589cbcab459606ea
obata <79499875+Obata-Hiromi@users.noreply.github.com>
2021-06-01 16:17:53 +0900

    Update 1.blade.php

--- resources/views/user/info_base/show/1.blade.php
+++ resources/views/user/info_base/show/1.blade.php
@@ -1,1 +1,72 @@
 ここにユーザの情報の表示するHTMLを書く
+    <h1>利用者情報</h1>
+<details>
+	<summary>基本情報</summary>		
+	<table>
+		<tr><td>氏名</td><td>：山田太郎</td></tr>	
+		<tr><td>性別</td><td>：男</td></tr>			
+		<tr><td>生年月日</td><td>：1950年7月9日</td></tr>	
+		<tr><td>住所</td><td>：〒xxx-xxxx　○○県○○市○○区○○町xx-xx</td></tr>	
+		<tr><td>電話番号</td><td>：(xxx)xxx-xxxx</td></tr>	
+		<tr><td>位置情報サービス</td><td>：ON</td></tr>		
+	</table>
+</details>
+
+<details>
+	<summary>家族情報</summary>		
+	<table>
+		<tr><td>氏名</td><td>：山田一郎</td></tr>	
+		<tr><td>性別</td><td>：男</td></tr>			
+		<tr><td>年齢</td><td>：23歳</td></tr>		
+		<tr><td>続柄</td><td>：息子</td></tr>		
+		<tr><td>住所</td><td>：〒xxx-xxxx　○○県○○市○○区○○町xx-xx</td></tr>	
+		<tr><td>電話番号</td><td>：(xxx)xxx-xxxx</td></tr>		
+		<tr><td>メールアドレス</td><td>：xxxxx@xxxxx.xxx</td></tr>		
+	</table>
+</details>
+
+<details>
+	<summary>住まい</summary>		
+	<table>
+		<tr><td>同居人の有無</td><td>：有</td></tr>			
+		<tr><td>最寄りの避難場所</td><td>：○○</td></tr>	
+	</table>
+</details>
+
+<details>
+	<summary>医療</summary>
+	<table>
+		<tr><td>平熱</td><td>：36.2</td></tr>		
+		<tr><td>身長</td><td>：170.0</td></tr>		
+		<tr><td>体重</td><td>：61kg</td></tr>		
+	  <tr><th>薬の使用状況</th><td></td></tr>
+		<tr><td>アレルギー歴</td><td>：○○、○○</td></tr>	
+		<tr><td>既往歴</td><td>：○○</td></tr>				
+		<tr><td>手術歴</td><td>：○○</td></tr>				
+		<tr><td>かかりつけの病院</td><td>：○○</td></tr>	
+	</table>
+</details>
+
+<details>
+	<summary>福祉</summary>
+	<table>
+		<tr><td>障害の有無</td><td>：聴覚、視覚、右足</td></tr>		
+		<tr><td>要支援・要介護認定</td><td>：有</td></tr>			
+		<tr><td>介護者の有無と続柄</td><td>：有（サービス提供者）</td></tr>		
+		<tr><td>介護サービスの有無</td><td>：有</td></tr>		
+		<tr><td>サービス内容</td><td>：在宅サービス</td></tr>		
+		<tr><td>利用サービス施設名称</td><td>：横浜市福祉サービス協会戸塚介護事務所</td></tr>	
+		<tr><td>在宅酸素療法</td><td>：無</td></tr>		
+		<tr><td>介助者</td><td>：有</td></tr>			
+	</table>
+</details>
+
+<details>
+	<summary>使用するアプリ内容</summary>
+	<table>
+		<tr><td>平常時安否確認</td><td>：利用しない</td></tr>	
+		<tr><td>災害時安否確認</td><td>：通常版</td></tr>		
+		<tr><td>健康管理</td><td>：利用する</td></tr>			
+	</table>
+</details>
+
